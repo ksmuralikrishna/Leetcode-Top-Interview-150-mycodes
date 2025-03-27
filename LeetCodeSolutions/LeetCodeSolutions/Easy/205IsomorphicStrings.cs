7d7d1efaac51a321f9048730ec976f5ebc547ff0
Ksm <the13mk@gmail.com>
2025-03-27 22:52:59 +0530

    HashMap

---
+++ LeetCodeSolutions/LeetCodeSolutions/Easy/205IsomorphicStrings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Easy
+{
+    public class Solution
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length == 0 && t.Length == 0) return false;
+
+            var Dic = new Dictionary<char, char>();
+            var mappedChar = new HashSet<char>();
+            
+            for(int i=0; i < s.Length; i++)
+            {
+                if (Dic.ContainsKey(s[i]))
+                {
+                    if (Dic[s[i]] != t[i]) return false;
+                }
+                else
+                {
+                    if (mappedChar.Contains(t[i])) return false;
+                    mappedChar.Add(t[i]);
+                    Dic[s[i]] = t[i]; 
+                }
+
+            }
+            return true;
+            
+            
+        }
+    }
+
+}
---
+++ LeetCodeSolutions/LeetCodeSolutions/Easy/290WordPattern.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Easy
+{
+    public class WordPattern
+    {
+        public bool fnWordPattern(string pattern, string s)
+        {
+            string[] words = s.Split(' ');
+            if (words.Length != pattern.Length) return false;
+            var Dic = new Dictionary<char, string>();
+            var MappedPattern = new HashSet<string>();
+            for(int i = 0; i < pattern.Length; i++)
+            {
+                char character = pattern[i];
+                if (Dic.ContainsKey(pattern[i]))
+                {
+                    if (Dic[pattern[i]] != words[i]) return false;
+                }
+                else
+                {
+                    if (MappedPattern.Contains(words[i])) return false;
+                    MappedPattern.Add(words[i]);
+                    Dic[pattern[i]] = words[i];
+                }
+            }
+            
+            return true;
+        }
+    }
+}
