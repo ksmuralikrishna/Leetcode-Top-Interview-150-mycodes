f78f6ae91e47f08c259569b34c9128b9d7f46599
Ksm <the13mk@gmail.com>
2025-04-02 23:51:14 +0530

    Array/String

---
+++ LeetCodeSolutions/LeetCodeSolutions/Easy/58LengthofLastWord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Easy
+{
+    internal class _58LengthofLastWord
+    {
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            Console.WriteLine("s :" + s);
+            int lastIndex = s.LastIndexOf(' ');
+            Console.WriteLine("lastIndex :" + lastIndex);
+
+            return s.Length - lastIndex - 1;
+        }
+    }
+}
