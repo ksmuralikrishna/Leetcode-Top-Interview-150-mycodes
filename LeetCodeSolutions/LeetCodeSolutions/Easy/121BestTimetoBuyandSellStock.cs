831b71836fa994976882fd6fd1fe8c7ea325dc63
Ksm <the13mk@gmail.com>
2025-04-02 23:42:23 +0530

    Array/String

---
+++ LeetCodeSolutions/LeetCodeSolutions/Easy/121BestTimetoBuyandSellStock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Easy
+{
+    internal class _121BestTimetoBuyandSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int buy_price = prices[0], current_profit = 0, profit = 0;
+            foreach (var price in prices)
+            {
+                if (price < buy_price)
+                {
+                    buy_price = price;
+                }
+                current_profit = price - buy_price;
+                if (current_profit > profit)
+                {
+                    profit = current_profit;
+                }
+            }
+            return profit;
+        }
+    }
+}
