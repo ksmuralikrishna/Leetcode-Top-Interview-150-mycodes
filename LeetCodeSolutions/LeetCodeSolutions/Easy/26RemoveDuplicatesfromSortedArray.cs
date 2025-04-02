1b07ebef2c817cc7ad40eecc9dd0386d9b05e5e2
Ksm <the13mk@gmail.com>
2025-04-02 23:50:14 +0530

    Array/String

---
+++ LeetCodeSolutions/LeetCodeSolutions/Easy/26RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Easy
+{
+    internal class _26RemoveDuplicatesfromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0; // Edge case: empty array
+
+            int k = 1; // Pointer for unique elements
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    nums[k] = nums[i]; // Place unique element at index k
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+}
