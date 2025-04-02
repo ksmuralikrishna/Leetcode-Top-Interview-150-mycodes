3452bdd2421c7106a346a03a116f86c2ea9cfe9c
Ksm <the13mk@gmail.com>
2025-04-02 23:49:15 +0530

    Array/String

---
+++ LeetCodeSolutions/LeetCodeSolutions/Easy/169MajorityElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Easy
+{
+    internal class _169MajorityElement
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int count = 0, candidate = 0;
+            foreach (int num in nums)
+            {
+                if (count == 0)
+                {
+                    candidate = num;
+                }
+                if (num == candidate)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+            return candidate;
+        }
+    }
+}
