6f975cc08716bf5f87235f8dd81ef8c9f2ba72d0
Muralikrishna K S <87901954+ksmuralikrishna@users.noreply.github.com>
2025-07-22 23:46:10 +0530

    Added algo

--- 88MergeSortedArray.cs
+++ 88MergeSortedArray.cs
@@ -6,7 +6,7 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
         int count = 0, candidate = 0;
-
+ 
         foreach (int num in nums) { 
             if (count == 0) {
                 candidate = num;
